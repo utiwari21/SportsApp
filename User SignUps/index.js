e3a979cb3cb3ae2101a9f8ab4be70d74d16c7f8a
PromoterMotor-UtkarshTiwari <utkarsh.tiwari.promotermotor@gmail.com>
2025-12-16 11:43:40 -0500

    Added sessions and unignored package.json

--- User SignUps/index.js
+++ User SignUps/index.js
@@ -4,6 +4,7 @@ import crypto from "crypto";
 import { PrismaClient } from "@prisma/client";
 
 import nodemailer from "nodemailer";
+import path from "path";
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -20,6 +21,24 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//------------
+// Sessions
+//------------
+import session from "express-session";
+app.use(
+  session({
+    name: "sportsapp.sid",
+    secret: "dev-secret-change-later",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 // 1 hour
+    }
+  })
+);
+
 
 // --------------------
 // Signup route
@@ -84,14 +103,6 @@ app.post("/signup", async (req, res) => {
         <p>Please verify before logging in.</p>
     `);
 
-    res.redirect(
-        `/login.html?success=${encodeURIComponent(
-            "Email verified successfully! You can now log in."
-        )}`
-    );
-
-
-
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
@@ -127,7 +138,7 @@ app.get("/verify", async (req, res) => {
 // Login routes
 // --------------------
 app.get("/login", (req, res) => {
-  res.sendFile(new URL("./public/login.html", import.meta.url));
+  res.sendFile(path.resolve("public/login.html"));
 });
 
 app.post("/login", async (req, res) => {
@@ -145,13 +156,45 @@ app.post("/login", async (req, res) => {
       return res.redirect(`/login.html?error=${encodeURIComponent("Incorrect password")}`);
     }
 
-    res.redirect(`/dashboard.html?username=${encodeURIComponent(user.username)}`);
+    req.session.userId = user.id;
+    req.session.username = user.username;
+
+    res.redirect("/dashboard");
+
 
   } catch (err) {
     console.error(err);
     res.redirect(`/login.html?error=${encodeURIComponent("Server error")}`);
   }
 });
+
+app.get("/dashboard", (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect("/login.html?error=Please log in");
+  }
+
+  res.sendFile(path.resolve("public/dashboard.html"));
+});
+
+//fetching from dashboard.html
+app.get("/me", (req, res) => {
+  if (!req.session.userId) {
+    return res.status(401).json({ error: "Not logged in" });
+  }
+
+  res.json({
+    username: req.session.username
+  });
+});
+
+//logout route
+app.post("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/login.html");
+  });
+});
+
+
 
 // --------------------
 // Start server
